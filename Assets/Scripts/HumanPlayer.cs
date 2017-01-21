9293505dc44a114d88fef1873eddbc90be62b3ea
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2017-01-21 03:28:17 +0300

    Added Some more Scripts Barebone

---
+++ Assets/Scripts/HumanPlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanPlayer : MonoBehaviour {
+
+    public float Food = 100;
+    public float Health = 100;
+    public float Water = 100;
+
+    void Update()
+    {
+        ImHuman();
+    }
+
+    void ImHuman()
+    {
+        if (Health < 0) Die();
+        if (Water < 0) Thirsty();
+        if (Food < 0) Starve();
+    }
+
+    void Starve()
+    {
+
+
+
+    }
+
+    void Thirsty()
+    {
+
+
+    }
+
+    void Die()
+    {
+
+
+
+    }
+
+}
