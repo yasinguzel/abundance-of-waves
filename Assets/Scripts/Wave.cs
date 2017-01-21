fffca79ec03e707d05af892bbd98df002d99a4a5
guzelyasin <guzelyasin@icloud.com>
2017-01-21 22:25:23 +0300

    random items added

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wave : MonoBehaviour {
-  public  Vector3 beginPos;
-  public  Vector3 endPos;
-  public float speed = 2;
-  public GameObject healthBox;	
+  	public  Vector3 beginPos;
+  	public  Vector3 endPos;
+ 	public float speed = 2;
+	public GameObject[] spawn;
+	public int boxCount = 6;
+
 	public bool isUp = false;
 
 	// Use this for initialization
@@ -36,8 +38,8 @@ public class Wave : MonoBehaviour {
 	void makeBox(){
 		int[] array={-8,-5,-2,1,4,7}; //Gün sayısı değiştikçe eleman sayısı artacak(Geçerli değerlerden)
 
-		for (int i = 0; i < 6; i++) {
-				Instantiate (healthBox, new Vector3 (array [Random.Range (0, 6)], transform.position.y, transform.position.z), transform.rotation);
+		for (int i = 0; i < boxCount; i++) {
+			Instantiate (spawn[Random.Range(0,5)], new Vector3 (array [Random.Range (0, 6)], transform.position.y, transform.position.z), transform.rotation);
 			}
 		}
 
