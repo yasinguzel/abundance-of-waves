c4666b3d8347c829c6e7db70f9315f504de59672
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2017-01-21 03:13:02 +0300

    Git İgnore, Jobs

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	//yasin
+	
+	// Update is called once per frame
+	void Update () {
+        //Input.GetAxis("Horizontal");
+
+    }
+}
