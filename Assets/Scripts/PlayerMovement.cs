3315be047e86b88cf2f77e350f2900f0c2392ee3
guzelyasin <guzelyasin@icloud.com>
2017-01-21 05:10:59 +0300

    player movement added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,32 @@ public class PlayerMovement : MonoBehaviour {
 	//yasin
 	
 	// Update is called once per frame
-	void Update () {
-        //Input.GetAxis("Horizontal");
+	public int speed = 5;
 
-    }
+	void Update ()
+	{
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+
+		var pos = Camera.main.WorldToScreenPoint(transform.position);
+		var dir = Input.mousePosition - pos;
+		var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); 
+
+	
+	}
 }
