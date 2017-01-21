fe8bd1f655b7f3b9b052096d07cf5d5881b984df
guzelyasin <guzelyasin@icloud.com>
2017-01-21 11:34:06 +0300

    wave animation added

---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+  public  Vector3 beginPos;
+  public  Vector3 endPos;
+  public float speed = 2;
+  Animator anim;
+  SpriteRenderer render;
+
+	// Use this for initialization
+	void Start () {
+		render = GetComponent<SpriteRenderer>();
+	    anim = GetComponent<Animator>();
+	    anim.SetTrigger("Wave");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    if (this.transform.position.y < endPos.y)
+    {
+        this.transform.Translate(Vector3.up*Time.deltaTime*speed);
+    }
+    else
+    {
+        this.transform.position = beginPos;
+			anim.SetTrigger("Wave");
+    }
+	}
+}
