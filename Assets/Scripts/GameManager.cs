7716e59ac012aa45423ae6b5bde2b444ca0a347d
guzelyasin <guzelyasin@icloud.com>
2017-01-22 16:51:33 +0300

    yes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement ;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+  
+  public Image fadeMask;
+
+  private bool starting = false;
+  private float fadeTime;
+	// Use this for initialization
+	void Start () {
+		starting = false;
+    fadeTime = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (starting)
+    {
+      if(fadeTime < 1)
+      {
+        fadeTime += Time.deltaTime;
+        fadeMask.color = new Color(0,0,0,fadeTime);
+      }
+      else
+      {
+        SceneManager.LoadScene(2);
+      }
+    }
+	}
+
+  public void startButtonPressed()
+  {
+  Debug.Log("adad");
+    starting = true;
+  }
+}
---
+++ Assets/Scripts/ggj.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement ;
+
+public class ggj : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds(2);
+		SceneManager.LoadScene(1);
+	}
+	
+	// Update is called once per frame
+
+}
